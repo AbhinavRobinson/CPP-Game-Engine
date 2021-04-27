c9b36ea9d7841381ec95271b6c884ed901a30259
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-27 21:23:11 +0530

    (feat): trim bulk

--- Main.cpp
+++ Main.cpp
@@ -2,71 +2,48 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-/// <summary>
 /// Program Starts here! 
-/// 
 /// Creates a GLFW Window.
-/// </summary>
 int main() {
 	glfwInit();
 
-	/// <summary>
 	/// Tell GLFW about the enviorment.
-	/// </summary>
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	/// <summary>
 	/// Create Window of size 800x800.
-	/// </summary>
-	/// <returns>GLFW Window pointer</returns>
+	/// GLFW Window pointer
 	GLFWwindow* window = glfwCreateWindow(800, 800, "CPP-GameEngine", NULL, NULL);
 	
-	/// <summary>
 	/// If window is not created, exit program.
-	/// </summary>
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 
-	/// <summary>
 	/// If window is made, show it!
-	/// </summary>
 	glfwMakeContextCurrent(window);
 
-	/// <summary>
 	/// Init GLAD.
-	/// </summary>
 	gladLoadGL();
 
-	/// <summary>
 	/// Create Viewport with window dimensions.
-	/// </summary>
 	glViewport(0, 0, 800, 800);
 
-	/// <summary>
 	/// Repaint Window with dark coral blue color.
-	/// </summary>
 	glClearColor(0.0f, 0.13f, 0.17f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glfwSwapBuffers(window);
 
-	/// <summary>
 	/// Keep window open until closed by user.
-	/// </summary>
 	while (!glfwWindowShouldClose(window)) {
-		/// <summary>
 		/// Respond to Events.
-		/// </summary>
 		glfwPollEvents();
 	}
 
-	/// <summary>
 	/// Cleanup GLFW context.
-	/// </summary>
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
