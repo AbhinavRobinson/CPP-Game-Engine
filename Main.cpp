d68a51503c007fb4a6085e8dcb9b2411bdba1309
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-27 21:29:21 +0530

    (feat): vertex and fragment shaders

--- Main.cpp
+++ Main.cpp
@@ -2,48 +2,65 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-/// Program Starts here! 
-/// Creates a GLFW Window.
+// Vertex Shader source code
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+//Fragment Shader source code
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
+// Program Starts here! 
+// Creates a Window.
 int main() {
+	// Init GLFW.
 	glfwInit();
 
-	/// Tell GLFW about the enviorment.
+	// Tell GLFW about the enviorment.
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	/// Create Window of size 800x800.
-	/// GLFW Window pointer
+	// Create Window of size 800x800.
 	GLFWwindow* window = glfwCreateWindow(800, 800, "CPP-GameEngine", NULL, NULL);
 	
-	/// If window is not created, exit program.
+	// If window is not created, exit program.
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 
-	/// If window is made, show it!
+	// If window is made, show it!
 	glfwMakeContextCurrent(window);
 
-	/// Init GLAD.
+	// Init GLAD.
 	gladLoadGL();
 
-	/// Create Viewport with window dimensions.
+	// Create Viewport with window dimensions.
 	glViewport(0, 0, 800, 800);
 
-	/// Repaint Window with dark coral blue color.
+	// Repaint Window with dark coral blue color.
 	glClearColor(0.0f, 0.13f, 0.17f, 1.0f);
+	// feed data into buffer.
 	glClear(GL_COLOR_BUFFER_BIT);
+	// Swap back and front buffers.
 	glfwSwapBuffers(window);
 
-	/// Keep window open until closed by user.
+	// Keep window open until closed by user.
 	while (!glfwWindowShouldClose(window)) {
-		/// Respond to Events.
+		// Respond to Events.
 		glfwPollEvents();
 	}
 
-	/// Cleanup GLFW context.
+	// Cleanup GLFW context.
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
