a2c6f0cb0e61566994cd94538a7e93ef70b876bc
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-28 11:26:54 +0530

    (feat): Create Triangle out of Elements

--- Main.cpp
+++ Main.cpp
@@ -90,17 +90,26 @@ int main() {
 
 	// Create a Vertex Array Object (an array of buffers).
 	// Create a Vertex Buffer Object (to transfer data between CPU and GPU in batch).
-	GLuint VAO, VBO;
+	// Create a Element Array Buffer to store indices.
+	GLuint VAO, VBO, EBO;
 
 	// Our VAO has 1 Buffer object.
 	glGenVertexArrays(1, &VAO);
 	// Generate buffer with no. of objects, refernce to buffer.
 	// pass arg "1" since we only have one 3D object.
 	glGenBuffers(1, &VBO);
+	// Generate a buffer for indices.
+	glGenBuffers(1, &EBO);
+
 	// Tell GL about the VAO of buffer.
 	glBindVertexArray(VAO);
 	// Bind object as Current Object (GL Current object is target for GL functions).
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// Bind the EBO.
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+	// Create Link with indices array.
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	// Provide data to buffer.
 	// GL_STATIC_DRAW : Current Data (singular) will be used to draw images on screen multiple times.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
@@ -114,9 +123,10 @@ int main() {
 	glVertexAttribPointer(0,3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// Bind the VBO and VAO so it's not accidently modified.
+	// Bind the VBO, VAO, EBO so it's not accidently modified.
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	// Repaint Window with dark coral blue color.
 	glClearColor(0.0f, 0.13f, 0.17f, 1.0f);
@@ -138,7 +148,7 @@ int main() {
 		// Bind the Buffer.
 		glBindVertexArray(VAO);
 		// Draw Triangle :)
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
 		// Swap back and front buffers.
 		glfwSwapBuffers(window);
@@ -147,9 +157,10 @@ int main() {
 	}
 
 	// ---------------- MEMORY CLEANUP ----------------
-	// Delete VBO and VAO.
+	// Delete VBO, VAO and EBO.
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	// Delete Shader.
 	glDeleteProgram(shaderProgram);
 	// Cleanup GLFW context.
