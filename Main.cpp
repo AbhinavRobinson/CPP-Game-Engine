2fa9683d3e5129689de901d79274748ea812b3cf
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-28 11:50:19 +0530

    (feat): add VBO

--- Main.cpp
+++ Main.cpp
@@ -55,28 +55,23 @@ int main() {
 	// Create a Vertex Array Object (an array of buffers).
 	// Create a Vertex Buffer Object (to transfer data between CPU and GPU in batch).
 	// Create a Element Array Buffer to store indices.
-	GLuint VAO, VBO, EBO;
+	GLuint VAO, EBO;
 
 	// Our VAO has 1 Buffer object.
 	glGenVertexArrays(1, &VAO);
-	// Generate buffer with no. of objects, refernce to buffer.
-	// pass arg "1" since we only have one 3D object.
-	glGenBuffers(1, &VBO);
+	
 	// Generate a buffer for indices.
 	glGenBuffers(1, &EBO);
 
 	// Tell GL about the VAO of buffer.
 	glBindVertexArray(VAO);
-	// Bind object as Current Object (GL Current object is target for GL functions).
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	
 	// Bind the EBO.
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
 	// Create Link with indices array.
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	// Provide data to buffer.
-	// GL_STATIC_DRAW : Current Data (singular) will be used to draw images on screen multiple times.
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
 
 	// Define the VAO with the VBO.
 	// 0 is the location of the Vertex.
@@ -88,7 +83,6 @@ int main() {
 	glEnableVertexAttribArray(0);
 
 	// Bind the VBO, VAO, EBO so it's not accidently modified.
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
@@ -121,7 +115,6 @@ int main() {
 	// ---------------- MEMORY CLEANUP ----------------
 	// Delete VBO, VAO and EBO.
 	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
 	glDeleteBuffers(1, &EBO);
 	// Cleanup GLFW context.
 	glfwDestroyWindow(window);
---
+++ VBO.cpp
@@ -0,0 +1,28 @@
+#include"VBO.h"
+
+VBO::VBO(GLfloat* vertices, GLsizeiptr size) 
+{
+	// Generate buffer with no. of objects, refernce to buffer.
+	// pass arg "1" since we only have one 3D object.
+	glGenBuffers(1, &ID);
+	// Bind object as Current Object (GL Current object is target for GL functions).
+	glBindBuffer(GL_ARRAY_BUFFER, ID);
+	// Provide data to buffer.
+	// GL_STATIC_DRAW : Current Data (singular) will be used to draw images on screen multiple times.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+void VBO::Bind()
+{
+	glBindBuffer(GL_ARRAY_BUFFER, ID);
+}
+
+void VBO::Unbind()
+{
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void VBO::Delete()
+{
+	glDeleteBuffers(1, &ID);
+}
