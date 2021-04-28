a8043f3f23927628bfc30b0aa7c220c85e3a7bd4
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-28 11:20:47 +0530

    (feat): add new indices

--- Main.cpp
+++ Main.cpp
@@ -28,12 +28,23 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Vertices coordinates for equalateral triangle
+	// Vertices coordinates for equalateral triangle with inner divisions
 	GLfloat vertices[] =
 	{
 		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
 		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner Left
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner Right
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner Down
+	};
+
+	// Indices to create divisions and not waste Render power of invisible tris
+	GLuint indices[] =
+	{
+		0,3,5, // Lower left Triangle
+		3,2,4, // Lower Right Triangle
+		5,4,1 // Upper Triangle
 	};
 
 	// Create Window of size 800x800.
@@ -94,7 +105,7 @@ int main() {
 	// GL_STATIC_DRAW : Current Data (singular) will be used to draw images on screen multiple times.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// Link VAO with the VBO.
+	// Define the VAO with the VBO.
 	// 0 is the location of the Vertex.
 	// 3 is the number of floats per Vertex.
 	// GL_FLOAT is the datatype, GL_FALSE since we are not using normalized integers.
@@ -113,6 +124,8 @@ int main() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	// Swap back and front buffers.
 	glfwSwapBuffers(window);
+
+	// ---------------- RENDER LOOP ----------------
 
 	// Keep window open until closed by user.
 	while (!glfwWindowShouldClose(window)) {
