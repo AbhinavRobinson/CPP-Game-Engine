a86571f7052a0adaceb828b907e20784089b2a65
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-04-29 20:27:02 +0530

    (feat): make square

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
 
 #include"shaderClass.h"
 #include"VAO.h"
@@ -9,21 +10,18 @@
 
 // Vertices coordinates
 GLfloat vertices[] =
-{ //                COORDINATES                    |       COLOR        //
-	-0.5f    , -0.5f * float(sqrt(3)) / 3, 0.0f    , 0.8f, 0.3f, 0.2f  ,// Lower left corner
-	0.5f     , -0.5f * float(sqrt(3)) / 3, 0.0f    , 0.8f, 0.3f, 0.2f  ,// Lower right corner
-	0.0f     , 0.5f * float(sqrt(3)) * 2 / 3, 0.0f , 1.0f, 0.6f, 0.32f ,// Upper corner
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f     , 0.9f, 0.45f, 0.17f,// Inner left
-	0.5f / 2 , 0.5f * float(sqrt(3)) / 6, 0.0f     , 0.9f, 0.45f, 0.17f,// Inner right
-	0.0f     , -0.5f * float(sqrt(3)) / 3, 0.0f    , 0.8f, 0.3f, 0.2f   // Inner down
+{ //       COORDINATES       |       COLOR          //
+	-0.5f , -0.5f , 0.0f      , 1.0f, 0.0f , 0.0f  ,// Lower left corner
+	-0.5f ,  0.5f , 0.0f      , 0.0f, 1.0f , 0.0f  ,// Upper left corner
+	 0.5f ,  0.5f , 0.0f      , 0.0f, 0.0f , 1.0f  ,// Upper right corner
+	 0.5f , -0.5f , 0.0f      , 1.0f, 1.0f , 1.0f  ,// Lower right corner
 };
 
 // Indices for vertices order
 GLuint indices[] =
 {
-	0, 3, 5, // Lower left triangle
-	3, 2, 4, // Lower right triangle
-	5, 4, 1 // Upper triangle
+	0, 2, 1, // Upper triangle
+	0, 3, 2  // Lower triangle
 };
 
 // Program Starts here!
